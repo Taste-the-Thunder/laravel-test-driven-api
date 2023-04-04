982808c001c44c1a226c62dce1223481b060f5e5
dhruvinprajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-04-04 19:26:10 +0530

    first success test, remove feature & unit example file remove

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    public function test_store_todo_list(): void
+    {
+       // preperation / prepare
+
+       // action / perform
+       $response = $this->getJson(route('todo-list.store'));
+
+       // assertion / predict
+       $this->assertEquals(1,count($response->json()));
+    }
+}
